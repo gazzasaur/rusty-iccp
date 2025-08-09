68ef8d44bcdfcbd1581b933edb9c2868d5f4e096
gaz <30276587+gazzasaur@users.noreply.github.com>
2025-08-09 10:46:04 +1000

    Filling in more of the model and parser

--- rusty-cotp/src/model.rs
+++ rusty-cotp/src/model.rs
@@ -24,6 +24,59 @@ impl TransportProtocolDataUnit {
     }
 }
 
+pub struct ConnectionRequest {
+    source_reference: u16,
+    destination_reference: u16,
+    preferred_class: ConnectionClass,
+    options: Vec<ConnectionOption>,
+    parameters: Vec<CotpParameter>,
+    user_data: Vec<u8>,
+}
+
+impl ConnectionRequest {
+    pub fn new(
+        source_reference: u16,
+        destination_reference: u16,
+        preferred_class: ConnectionClass,
+        options: Vec<ConnectionOption>,
+        parameters: Vec<CotpParameter>,
+        user_data: &[u8],
+    ) -> TransportProtocolDataUnit {
+        TransportProtocolDataUnit::CR(Self {
+            source_reference,
+            destination_reference,
+            preferred_class,
+            options,
+            parameters,
+            user_data: user_data.into(),
+        })
+    }
+
+    pub fn source_reference(&self) -> u16 {
+        self.source_reference
+    }
+
+    pub fn destination_reference(&self) -> u16 {
+        self.destination_reference
+    }
+
+    pub fn preferred_class(&self) -> &ConnectionClass {
+        &self.preferred_class
+    }
+
+    pub fn options(&self) -> &[ConnectionOption] {
+        &self.options
+    }
+
+    pub fn parameters(&self) -> &[CotpParameter] {
+        &self.parameters
+    }
+
+    pub fn user_data(&self) -> &[u8] {
+        &self.user_data
+    }
+}
+
 pub enum ConnectionClass {
     Class0,
     Unknown(u8),
@@ -46,32 +99,13 @@ pub fn parse_connection_options(connection_options: u8) -> Vec<ConnectionOption>
     vec![ConnectionOption::Unknown(connection_options)]
 }
 
-pub struct ConnectionRequest {
-    source_reference: u16,
-    destination_reference: u16,
-    preferred_class: ConnectionClass,
-    options: Vec<ConnectionOption>,
-    parameters: Vec<CotpParameter>,
-    user_data: Vec<u8>,
-}
+pub struct ConnectionConfirm {}
 
-impl ConnectionRequest {
-    pub fn new(source_reference: u16, destination_reference: u16, preferred_class: ConnectionClass, options: Vec<ConnectionOption>, parameters: Vec<CotpParameter>, user_data: &[u8]) -> TransportProtocolDataUnit {
-        TransportProtocolDataUnit::CR(Self { source_reference, destination_reference, preferred_class, options, parameters, user_data: user_data.into() })
-    }
-}
+pub struct DisconnectRequest {}
 
-pub struct ConnectionConfirm {
-}
+pub struct Data {}
 
-pub struct DisconnectRequest {
-}
-
-pub struct Data {
-}
-
-pub struct TpduError {
-}
+pub struct TpduError {}
 
 pub enum CotpParameter {
     TpduLengthParameter(TpduLength),
@@ -99,7 +133,7 @@ impl From<u8> for TpduLength {
             0b00001011 => Self::Size2048,
             0b00001100 => Self::Size4096,
             0b00001101 => Self::Size8192,
-            x => Self::Unknown(x)
+            x => Self::Unknown(x),
         }
     }
 }
