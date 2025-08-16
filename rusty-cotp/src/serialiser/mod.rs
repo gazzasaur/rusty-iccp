d9acba85c93f337ffbe7ef7b23019b9697f0d02b
Gaz <30276587+gazzasaur@users.noreply.github.com>
2025-08-17 00:36:01 +1000

    lint

--- rusty-cotp/src/serialiser/mod.rs
+++ rusty-cotp/src/serialiser/mod.rs
@@ -1,5 +1,5 @@
 pub mod packet;
 pub mod packet_cc;
 pub mod packet_cr;
-pub mod params;
 pub mod packet_dt;
+pub mod params;
--- rusty-cotp/src/serialiser/packet_dt.rs
+++ rusty-cotp/src/serialiser/packet_dt.rs
@@ -1,7 +1,9 @@
 use crate::{
     api::CotpError,
     packet::{
-        connection_request::{ConnectionRequest, CONNECTION_REQUEST_CODE}, data_transfer::{DataTransfer, DATA_TRANSFER_CODE}, parameter::{ConnectionClass, ConnectionOption, CotpParameter, TpduSize}
+        connection_request::{CONNECTION_REQUEST_CODE, ConnectionRequest},
+        data_transfer::{DATA_TRANSFER_CODE, DataTransfer},
+        parameter::{ConnectionClass, ConnectionOption, CotpParameter, TpduSize},
     },
     serialiser::params::serialise_parameters,
 };
@@ -34,14 +36,8 @@ mod tests {
     async fn parse_payloads_happy() -> Result<(), anyhow::Error> {
         let subject = TransportProtocolDataUnitSerialiser::new();
 
-        assert_eq!(
-            subject.serialise(&TransportProtocolDataUnit::DT(DataTransfer::new(false, &[1, 2, 3])))?,
-            hex::decode("02F000010203")?.as_slice()
-        );
-        assert_eq!(
-            subject.serialise(&TransportProtocolDataUnit::DT(DataTransfer::new(true, &[3, 2, 1])))?,
-            hex::decode("02F080030201")?.as_slice()
-        );
+        assert_eq!(subject.serialise(&TransportProtocolDataUnit::DT(DataTransfer::new(false, &[1, 2, 3])))?, hex::decode("02F000010203")?.as_slice());
+        assert_eq!(subject.serialise(&TransportProtocolDataUnit::DT(DataTransfer::new(true, &[3, 2, 1])))?, hex::decode("02F080030201")?.as_slice());
 
         Ok(())
     }
