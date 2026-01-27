6bb52ab5e49e7446454803f35a9fa0bb051aba6c
Gaz <30276587+gazzasaur@users.noreply.github.com>
2026-01-27 15:15:30 +1000

    Added getnamedvariablelistattributesresponse

--- rusty-mms/src/pdu/getnamedvariablelistattributesresponse.rs
+++ rusty-mms/src/pdu/getnamedvariablelistattributesresponse.rs
@@ -25,23 +25,13 @@ pub(crate) fn parse_get_named_variable_list_attributes_response(payload: &Any<'_
             Some([161]) => {
                 list_of_variables = {
                     let items = process_constructed_data(item.data).map_err(to_mms_error("Failed to parse MMS DefineNamedVariableList PDU"))?;
-                    let mut items_iter = items.iter();
-
-                    let variable_specifications_payload = items_iter
-                        .next()
-                        .ok_or_else(|| MmsError::ProtocolError("Failed to parse MMS DefineNamedVariableList PDU - No Variable Specifications found".into()))?;
                     let mut variable_specifications = vec![];
-                    for variable_specifications_item in process_constructed_data(variable_specifications_payload.data).map_err(to_mms_error("Failed to parse MMS DefineNamedVariableList PDU - Failed to parse Variable Specifications"))? {
-                        let variable_specifications_item_data = variable_specifications_item.to_der_vec()
-                            .map_err(to_mms_error("Failed to parse MMS DefineNamedVariableList PDU - Failed to parse Variable Specification"))?;
-                        let (_, unwrapped_variable_specifications_item) =
-                            parse_ber_any(&variable_specifications_item_data).map_err(to_mms_error("Failed to parse MMS DefineNamedVariableList PDU - Failed to parse List Of Variables Item Sequence"))?;
+                    for variable_specifications_item in items {
                         variable_specifications.push(ListOfVariablesItem::parse(
-                            &unwrapped_variable_specifications_item,
+                            &variable_specifications_item,
                             "Failed to parse MMS DefineNamedVariableList PDU - Failed to parse List Of Variables Item",
                         )?);
                     }
-
                     Some(variable_specifications)
                 }
             }
@@ -52,7 +42,7 @@ pub(crate) fn parse_get_named_variable_list_attributes_response(payload: &Any<'_
     let deletable = deletable.ok_or_else(|| MmsError::ProtocolError("No Deletable Flag on Get Variable Access Attributes Response PDU".into()))?;
     let list_of_variables = list_of_variables.ok_or_else(|| MmsError::ProtocolError("No Deletable Flag on Get Variable Access Attributes Response PDU".into()))?;
 
-    Ok(MmsConfirmedResponse::GetNamedVariableListAttributes { deletable, list_of_variables: vec![] })
+    Ok(MmsConfirmedResponse::GetNamedVariableListAttributes { deletable, list_of_variables })
 }
 
 pub(crate) fn get_named_variable_list_attributes_response_to_ber<'a>(deletable: bool, list_of_variables: &'a [ListOfVariablesItem]) -> Result<BerObject<'a>, MmsError> {
